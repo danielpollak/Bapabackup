8368a95862cf49e97980de4f51cff403cf0663c6
reiffev <reiffev@gmail.com>
2017-04-18 16:43:58 -0400

    Combined Blynk and Temboo into one ino file

--- Final/Final.ino
+++ Final/Final.ino
@@ -2,6 +2,8 @@
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <Temboo.h>
+#include "TembooAccount.h"
 WiFiClient client;
 
 char auth[] = "9b1b5f1de728419487c4dca8ef396462"; char ssid[] = "CS390IOT"; char pass[] = "12345678";
@@ -14,6 +16,7 @@ void setup() {
   Blynk.begin(auth, ssid, pass);
   Blynk.virtualWrite(V0, HIGH);
   Blynk.virtualWrite(V1, "clr");
+  scrapeWeather();
 }
 
 BLYNK_WRITE(V0){
@@ -35,4 +38,41 @@ BLYNK_WRITE(V2){
 
 void loop() {
   Blynk.run();
+}
+
+void scrapeWeather() {
+  TembooChoreo GetWeatherByAddressChoreo(client);
+   // Invoke the Temboo client
+  GetWeatherByAddressChoreo.begin();
+   // Set Temboo account credentials
+  GetWeatherByAddressChoreo.setAccountName(TEMBOO_ACCOUNT);
+  GetWeatherByAddressChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+  GetWeatherByAddressChoreo.setAppKey(TEMBOO_APP_KEY);
+  
+  // Set Choreo inputs
+  GetWeatherByAddressChoreo.addInput("Address", "Amherst, MA");
+  GetWeatherByAddressChoreo.addInput("Day", "1");
+  
+  // Identify the Choreo to run
+  GetWeatherByAddressChoreo.setChoreo("/Library/Yahoo/Weather/GetWeatherByAddress");
+    
+  //before run, set output filters (DAN)
+  String weatherCodePath = "/rss/channel/item/yweather:forecast/@code";
+  GetWeatherByAddressChoreo.addOutputFilter("weather", weatherCodePath, "Response");
+  
+  // Run the Choreo; when results are available, print them to serial
+  GetWeatherByAddressChoreo.run();
+  int code = 0;
+  while(GetWeatherByAddressChoreo.available()) { /*FROM HOW TO FILTER DATA*/
+    String name = GetWeatherByAddressChoreo.readStringUntil('\x1F'); name.trim(); // use “trim” to get rid of newlines
+    String code = GetWeatherByAddressChoreo.readStringUntil('\x1E'); code.trim(); code = code.toInt();
+    Serial.println(code);
+    if(name == "weather"){break;} // this leaves code being exactly what we need it to be.
+  }
+  if(code <= 12 || code == 17 || code == 18 || code == 35 || ( code >= 37 || code <= 40) || code == 45 || code == 47){
+    //do something
+  }
+
+  GetWeatherByAddressChoreo.close();
+  //delay(10000);
 }
