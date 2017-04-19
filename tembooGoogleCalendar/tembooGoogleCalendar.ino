0b8cfbedf3f634b0c5adca7bdbfc2f0a68871169
reiffev <reiffev@gmail.com>
2017-04-19 16:36:13 -0400

    Modified Google Calendar Temboo to have the basic Temboo  information

--- tembooGoogleCalendar/tembooGoogleCalendar.ino
+++ tembooGoogleCalendar/tembooGoogleCalendar.ino
@@ -2,7 +2,6 @@
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-//#include <WidgetRTC.h>
 #include <Temboo.h>
 #include "TembooAccount.h"
 WiFiClient client;
@@ -20,39 +19,17 @@ void loop() {
 }
 
 void scrapeWeather() {
-  TembooChoreo GetWeatherByAddressChoreo(client);
+  TembooChoreo SearchEvents(client);
    // Invoke the Temboo client
-  GetWeatherByAddressChoreo.begin();
+  SearchEvents.begin();
    // Set Temboo account credentials
-  GetWeatherByAddressChoreo.setAccountName(TEMBOO_ACCOUNT);
-  GetWeatherByAddressChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
-  GetWeatherByAddressChoreo.setAppKey(TEMBOO_APP_KEY);
-  
-  // Set Choreo inputs
-  GetWeatherByAddressChoreo.addInput("Address", "Amherst, MA");
-  GetWeatherByAddressChoreo.addInput("Day", "1");
+  SearchEvents.setAccountName(TEMBOO_ACCOUNT);
+  SearchEvents.setAppKeyName(TEMBOO_APP_KEY_NAME);
+  SearchEvents.setAppKey(TEMBOO_APP_KEY);
   
   // Identify the Choreo to run
-  GetWeatherByAddressChoreo.setChoreo("/Library/Yahoo/Weather/GetWeatherByAddress");
-    
-  //before run, set output filters (DAN)
-  String weatherCodePath = "/rss/channel/item/yweather:forecast/@code";
-  GetWeatherByAddressChoreo.addOutputFilter("weather", weatherCodePath, "Response");
-  
-  // Run the Choreo; when results are available, print them to serial
-  GetWeatherByAddressChoreo.run();
-  String code = "";
-  while(GetWeatherByAddressChoreo.available()) { /*FROM HOW TO FILTER DATA*/
-    String name = GetWeatherByAddressChoreo.readStringUntil('\x1F'); name.trim(); // use “trim” to get rid of newlines
-    code = GetWeatherByAddressChoreo.readStringUntil('\x1E');
-    if(name == "weather"){break;} // this leaves code being exactly what we need it to be.
-  }
-  code.trim(); int data = code.toInt();
-  Serial.print("code:"); Serial.println(code);
-  if(data <= 12 || data == 17 || data == 18 || data == 35 || ( data>= 37 || data<= 40) || data== 45 || data== 47){
-    //do something
-  }
+  SearchEvents.setChoreo("Library/Google/Calendar/SearchEvents");
 
-  GetWeatherByAddressChoreo.close();
+  SearchEvents.close();
 }
 
