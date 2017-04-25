75915b356d6ae778ca287e7e11fe6a6435337c8b
hamdanspam <danpollak23@gmail.com>
2017-04-25 15:43:07 -0400

    added debouncing

--- RFIDnewest/RFIDnewest.ino
+++ RFIDnewest/RFIDnewest.ino
@@ -36,37 +36,46 @@ http://bildr.org/2011/02/rfid-arduino/
 on rfid module, pins 1, 9, 11 go to GND, D2, and 5V, and pin 7 goes to pin 1 to set the format to ascii
 */
 //https://learn.sparkfun.com/tutorials/sparkfun-rfid-starter-kit-hookup-guide
+
+/* -- RFID init -- */
 #include <SoftwareSerial.h>
 #define RFIDINTERRUPTPIN 3
 // Choose two pins for SoftwareSerial
 SoftwareSerial rSerial(2, 7); // RX, TX
 // For SparkFun's tags, we will receive 16 bytes on every tag read, but throw four away. The 13th space will always be 0, since proper strings in Arduino end with 0
-
 // These constants hold the total tag length (tagLen) and the length of the part we want to keep (idLen),plus the total number of tags we want to check against (kTags)
 const int tagLen = 16;
-const int idLen = 13; char newTag[idLen]; // Empty array to hold a freshly scanned tag
+const int idLen = 13;
+char newTag[idLen]; // Empty array to hold a freshly scanned tag
 const int kTags = 4;
 
 // Put your known tags here!
 char knownTags[kTags][idLen] = {"7A005B0FF8D6"};
-
-void setup() {
-  // Starts the hardware and software serial ports
+void initRFID(){
    Serial.begin(9600);
    rSerial.begin(9600);
    pinMode(RFIDINTERRUPTPIN, INPUT);
    attachInterrupt(digitalPinToInterrupt(RFIDINTERRUPTPIN), rfidRead, RISING);
 }
+/* -- RFID init -- */
+
+void setup() {
+  initRFID();
+}
 
 void rfidRead(){
-  Serial.println("H");
-  int i = 0;// Counter for the newTag array
-  int readByte;// Variable to hold each byte read from the serial buffer
-  boolean tag = true;
+  boolean tag = false;
   // This makes sure the whole tag is in the serial buffer before
   // reading, the Arduino can read faster than the ID module can deliver!
   //  if (rSerial.available() == tagLen) {tag = true;}
+  if (rSerial.available() == tagLen) {
+    tag = true;
+  }
   if(tag) {
+    Serial.println("H");
+    int i = 0;// Counter for the newTag array
+    int readByte;// Variable to hold each byte read from the serial buffer
+  
     while (rSerial.available()) {
       // Take each byte out of the serial buffer, one at a time
       readByte = rSerial.read();
@@ -74,13 +83,10 @@ void rfidRead(){
 
       /* This will skip the first byte (2, STX, start of text) and the last three, ASCII 13, CR/carriage return, ASCII 10, LF/linefeed, and ASCII 3, ETX/end of text, leaving only the unique part of the tag string. It puts the byte into
       the first space in the array, then steps ahead one spot */
-      if (readByte != 2 && readByte!= 13 && readByte != 10 && readByte != 3) {
-        newTag[i] = readByte; i++;
-      }
-      // If we see ASCII 3, ETX, the tag is over
-      if (readByte == 3) {  tag = false; }
+      if (readByte != 2 && readByte!= 13 && readByte != 10 && readByte != 3) {newTag[i] = readByte; i++;}
+      if (readByte == 3) {  tag = false; }      // If we see ASCII 3, ETX, the tag is over
     }
-  }
+  
   // don't do anything if the newTag array is full of zeroes
   if (strlen(newTag)== 0) {return;}
   else
@@ -92,6 +98,7 @@ void rfidRead(){
 
   // Once newTag has been checked, fill it with zeroes to get ready for the next tag read
   for (int c=0; c < idLen; c++) {newTag[c] = 0;}
+  }
   }
 }
   
